1e1b4c7cfda62d8f77ba0771a2a5b157f06e7aec
amazingkit <157248067+amazingkit@users.noreply.github.com>
2024-05-10 10:19:28 -0700

    Update getData.js

--- getData.js
+++ getData.js
@@ -6,3 +6,41 @@ $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
         $('#astroNames').append('<li class="astronaut"><p><strong>Name:</strong> ' + d['name'] + '</p><p><strong>Craft:</strong> ' + d['craft'] + '</p></li>');
     });
 });
+
+
+
+
+"message": "success",
+"people": 
+    [
+    {
+     "name": "Jasmin Moghbeli",
+     "craft": "ISS"
+     },
+    {
+    "name": "Andreas Mogensen",
+    "craft": "ISS"
+    },
+    {
+    "name": "Satoshi Furukawa",
+    "craft": "ISS"
+    },
+    {
+    "name": "Konstantin Borisov",
+    "craft": "ISS"
+    },
+    {
+    "name": "Oleg Kononenko",
+    "craft": "ISS"
+    },
+    {
+    "name": "Nikolai Chub",
+    "craft": "ISS"
+    },
+    {
+    "name": "Loral O'Hara",
+    "craft": "ISS"
+    }
+    ],
+"number": 7
+}
